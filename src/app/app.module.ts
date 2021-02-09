8604dc4ba552b6fac0914190f0f80daf1ff7474e
Michael Zabala (Mike) <wonkaphreak@gmail.com>
2021-02-09 01:09:43 -0400

    configure modules and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
+//* Imports - Modules
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from "@angular/common/http";
+import {FormsModule} from '@angular/forms';
+
+//* Declarations - Components
 import { AppComponent } from './app.component';
 import { EmployeeComponent } from './components/employee/employee.component';
 
+//* Providers - Services
+import { EmployeeService } from "./services/employee.service";
 @NgModule({
   declarations: [
     AppComponent,
     EmployeeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
